3079f27d1ee9b4cfa9af4bcbeec717e5bc9e42f2
Mallekedi Yamini <yaminimallekedi@gmail.com>
2026-07-22 20:50:24 +0530

    Add hello-service and update RabbitTest

--- auth-service/src/main/java/com/stschool/microservices/authservice/RabbitTest.java
+++ auth-service/src/main/java/com/stschool/microservices/authservice/RabbitTest.java
@@ -1,29 +1,29 @@
-//package com.stschool.microservices.authservice;
-//
-//import com.stschool.microservices.authservice.config.RabbitConfig;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class RabbitTest implements CommandLineRunner {
-//
-//    private final RabbitTemplate rabbitTemplate;
-//
-//    public RabbitTest(RabbitTemplate rabbitTemplate) {
-//        this.rabbitTemplate = rabbitTemplate;
-//    }
-//
-//    @Override
-//    public void run(String... args) {
-//        System.out.println("RabbitTemplate created successfully");
-//
-//        rabbitTemplate.convertAndSend(
-//                RabbitConfig.EXCHANGE,
-//                RabbitConfig.ROUTING_KEY,
-//                "Hello RabbitMQ!"
-//        );
-//
-//        System.out.println("Message sent successfully");
-//    }
-//}
+package com.stschool.microservices.authservice;
+
+import com.stschool.microservices.authservice.config.RabbitConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitTest implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitTest(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("RabbitTemplate created successfully");
+
+        rabbitTemplate.convertAndSend(
+                RabbitConfig.EXCHANGE,
+                RabbitConfig.ROUTING_KEY,
+                "Hello RabbitMQ!"
+        );
+
+        System.out.println("Message sent successfully");
+    }
+}
